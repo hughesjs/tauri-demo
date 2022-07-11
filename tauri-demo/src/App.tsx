e1d2eb72d934ac0c5b6fba7feaa7a2531d33fefd
James Hughes <james.hughes@codurance.com>
2022-07-11 22:39:01 +0100

    Added first rust command
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- tauri-demo/src/App.tsx
+++ tauri-demo/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { invoke } from '@tauri-apps/api';
 
 function App() {
   
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [greeting, setGreeting] = useState("Hello There!");
 
   return (
     <div className="App">
@@ -15,32 +17,19 @@ function App() {
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
+
+          <button type="button" onClick={() => invoke('greet',{'name': 'General Kenobi'}).then(t => setGreeting(t as string))}>
+            {greeting}
+          </button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
   )
 }
+
+
 
 export default App
